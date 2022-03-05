dd8794a5288d03ee41c1fff870f27beeb2d3511a
TrueRealHero <ub1nono@gmail.com>
2022-03-05 19:35:35 +0600

    Repeated quote logic and saving quotes in DB

--- kanyewest/ten_quotes/models.py
+++ kanyewest/ten_quotes/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-# class Quote(models.Model):
-#     kanye_quote = models.TextField('Kanye quote')
+class Quote(models.Model):
+    kanye_quote = models.TextField('Цитата Kanye', unique=True, null=True)
+    letters_amount = models.IntegerField('Количество букв в цитате', null=True)
+    # repeated_letters = models.TextField('Количество одинаковых букв', null=True)
+    # average_len = models.IntegerField('Средняя длинна слов', null=True)
+    # longest_words = models.TextField('Три длиннейших слова', null=True)
 
-#     def __str__(self):
-#         return "Kanye quotes"
+    def __str__(self):
+        return (str(self.kanye_quote))
 
-#     class Meta:
-#         verbose_name = 'Quote'
-#         verbose_name_plural = 'Quotes'
+    class Meta:
+        verbose_name = 'Quote'
+        verbose_name_plural = 'Quotes'
--- kanyewest/ten_quotes/views.py
+++ kanyewest/ten_quotes/views.py
@@ -1,8 +1,8 @@
-from urllib import response
-from django.shortcuts import render
+from enum import unique
+from operator import contains
+from django.shortcuts import render, redirect
 import requests
-from collections import defaultdict
-# from .models import Quote
+from .models import Quote
 
 # Create your views here.
 
@@ -10,46 +10,27 @@ def index(request):
     return render (request, 'index.html')
 
 def get(request):
-    quotes = []
-    letters_in_sentence = []
-    result = []
-    i = 0
-    j = 0
+    id_of_quotes = []
     for x in range(2):
-        quote = requests.get('https://api.kanye.rest/').json()
-        sentence = list(quote.values())
-        quotes.append(sentence)
+        quote = Quote()
+        json_quote = requests.get('https://api.kanye.rest/').json()
+        sentence = list(json_quote.values())
+        quote.kanye_quote = sentence
+        if Quote.objects.filter(kanye_quote__contains = sentence):
+            existing_quote = Quote.objects.get(kanye_quote__contains = sentence)
+            id_of_quotes.append(existing_quote.id)
+            continue
         letters_amount = int()
-        for y in sentence:
-            words = y.split()
+        for separate in sentence:
+            words = separate.split()
         for word in words:
-            z = len(word)
-            letters_amount += z
-        letters_in_sentence.append(letters_amount)
-    while i < len(quotes) and j < len(letters_in_sentence):
-        compilation = quotes[i], letters_in_sentence[j]
-        result.append(compilation)
-        i += 1
-        j += 1
+            word_len = len(word)
+            letters_amount += word_len
 
-    context = {'sentence':sentence, 'letters_amount':letters_amount, 'letters_in_sentence':letters_in_sentence,
-                'result':result}
+        quote.letters_amount = letters_amount
+        quote.save()
+        id_of_quotes.append(quote.id)
+
+    quotes = Quote.objects.filter(id__in = id_of_quotes)
+    context = {'quotes':quotes}
     return render (request, 'index.html', context)
-
-
-# a = requests.get('https://api.kanye.rest/').json()
-# # my_dict = defaultdict(set)
-# # for word in a.values.split():
-# #         my_dict[len(word)].add(word)   
-# print(a)
-# y = list(a.values())
-# for x in y:
-#     print(x.split())
-#     print(len(x))
-
-# f = 'The world is our office'
-# m = f.split()
-# j = int()
-# for x in m:
-#     k = len(x)
-#     j += k
